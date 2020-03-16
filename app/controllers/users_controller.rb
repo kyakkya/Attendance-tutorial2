b01e543fbfcc0a7bb13bb15bec97bf77e41cce02
akiko <inataaiko123@gmail.com>
2020-03-16 14:24:47 +0000

    8.1.3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,29 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-     log_in @user  # 保存成功後、ログインします。
-     flash[:success] = '新規作成に成功しました。'
-     redirect_to @user
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
     else
       render :new
     end
   end
   
   def edit
-    @user = user.find(params[:id])
-  end  
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    # 更新に成功した場合の処理を記述します。
+    flash[:success] = 'ユーザー情報を更新しました。'
+    redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
 
   private
 
