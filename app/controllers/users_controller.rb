b74357b48b597ccb72cf71429d4324167a4f27a0
akiko <inataaiko123@gmail.com>
2020-03-17 12:49:52 +0000

    8.1.3再確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,22 +18,20 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-    # 更新に成功した場合の処理を記述します。
-    flash[:success] = 'ユーザー情報を更新しました。'
-    redirect_to @user
+     flash[:success] = "ユーザー情報を更新しました。"
+     redirect_to @user 
     else
-      render :edit
+      render :edit      
     end
   end
-  
 
   private
 
