aea04c2cbb4794f8e71b4f9f74f37d408ad1cf1a
akiko <inataaiko123@gmail.com>
2020-05-02 02:30:26 +0000

    edit-attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,9 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
+  
 
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
@@ -49,4 +51,13 @@ class AttendancesController < ApplicationController
     params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
   end   
   
+  def  admin_or_correct_user
+    @user = User.find(params[:user_id]) if @user.blank?
+    unless current_user?(@user) || current_user.admin?
+     flash[:danger] = "編集権限がありません。"
+     redirect_to(root_url)
+    end
+  end   
 end
+
+
